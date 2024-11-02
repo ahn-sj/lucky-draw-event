460b6c58bf5482eac314fa62d17d48d6edb6e7a3
ahn-sj <jaesa5221@gmail.com>
2024-11-02 09:39:40 +0900

    Init: Init Project

---
+++ src/main/java/com/tally/luckydraw/LuckyDrawApplication.java
@@ -0,0 +1,13 @@
+package com.tally.luckydraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyDrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyDrawApplication.class, args);
+    }
+
+}
